1140775e27b060a0bcaa0b3c0410d6dd6a5ed7f6
Steve Sims <steve_sims7@yahoo.co.uk>
2021-01-08 16:09:48 +0000

    add prelininary search component

--- src/Venues/Search.js
+++ src/Venues/Search.js
@@ -1,5 +1,28 @@
+import { useQuery } from 'react-query';
+
 function Search({ forLocation }) {
-  return <div>Searching for {forLocation}</div>
+  const searchURL = new URL('https://uat.ospace.co.uk/api/v1/spaces/search');
+  searchURL.searchParams.append('location', forLocation);
+  const searchRequest = new Request(searchURL);
+
+  const { isLoading, error, data } = useQuery(`search-${forLocation}`, () =>
+    fetch(searchRequest).then(res => res.json())
+  );
+
+  if (error) {
+    return <div>An error has occurred {error.message}</div>;
+  }
+
+  return (<div>
+    <div>Searching for {forLocation}</div>
+    {isLoading ?
+      <div>Loading...</div>
+      :
+      <pre>
+        {JSON.stringify(data, undefined, 2)}
+      </pre>
+    } 
+  </div>);
 }
 
 export default Search
